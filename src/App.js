79d2d265fc0f9d0a7b02d94a45744a74b20ee85a
Melanie Thompson <melanie@ammonitewealth.com>
2021-08-31 12:16:21 +0100

    changes app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>React app running on AWS with Elastic Beanstalk, Travis CI with tests, and Docker!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
